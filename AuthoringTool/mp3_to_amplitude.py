9ceff0b59cb36860430a7d388646d3f4d2c704e5
gorengordon <curiosity@gorengordon.com>
2021-03-22 13:38:02 +0200

    -game 2 full script, before QA  in the lab

--- AuthoringTool/mp3_to_amplitude.py
+++ AuthoringTool/mp3_to_amplitude.py
@@ -92,6 +92,6 @@ def path_to_lip_csv(the_path):
 
 #path_to_lip_csv('sounds/Fuzzy_contest/wav')
 
-mp3_to_lip_csv('sounds/game_2/wav/', 'sounds/game_2/', 'that isnt a cow.wav')
+mp3_to_lip_csv('sounds/game_2/wav/', 'sounds/game_2/', 'now the cow wants to eat a lemon.wav')
 
 
